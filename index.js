52a4c560bd47a8ad5831e4d2e1b72d426134abc0
Eran <eran.hasarali.2001@gmail.com>
2025-03-12 13:20:36 +0530

    done

--- index.js
+++ index.js
@@ -7,13 +7,21 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.use("/", (req, res) => {
+  const date = new Date().getDay();
+
+  let type = "a weekday";
+  let adv = "it's time to work hard!";
+
+  if (date === 0 || date === 6) {
+    type = "a weekend";
+    adv = "it's time to work hard!";
+  }
+
   res.render("index.ejs", {
-    dayType: "weekend",
-    advice: "it's time to work hard!",
+    dayType: type,
+    advice: adv,
   });
 });
-
-console.log(new Date().getFullYear());
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
