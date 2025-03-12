66d34ed3ae940c69d4f26cc6eddecf4962b70bbc
Eran <eran.hasarali.2001@gmail.com>
2025-03-12 11:37:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use("/", (req, res) => {
+  res.render("/views/index.ejs", {
+    dayType: "a weekday",
+    advice: "it's time to work hard.",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}.`);
+});
