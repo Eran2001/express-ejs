d33000f9e3163daa4a9c74ce99850b037d2e3521
Eran <eran.hasarali.2001@gmail.com>
2025-03-12 14:34:22 +0530

    remove some

--- index.js
+++ index.js
@@ -7,22 +7,7 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.use("/", (req, res) => {
-  const myList = ["Apple", "Banana", "Cherry", "Date", "Elderberry"];
-
-  const date = new Date().getDay();
-
-  let type = "a weekday";
-  let adv = "it's time to work hard!";
-
-  if (date === 0 || date === 6) {
-    type = "a weekend";
-    adv = "it's time to work hard!";
-  }
-
-  res.render("index.ejs", {
-    dayType: type,
-    advice: adv,
-  });
+  res.render("index.ejs");
 });
 
 app.listen(port, () => {
