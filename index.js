de2fb44a161d75e187f23411e8d84feaf79aca64
Eran <eran.hasarali.2001@gmail.com>
2025-03-12 12:46:48 +0530

    add some

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 app.use("/", (req, res) => {
   res.render("index.ejs", {
     dayType: "weekend",
